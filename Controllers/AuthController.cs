c4c84b09131a5c329b440bc3eee92a763236ec9a
DESKTOP-2A3VE7I\lavan <lavanya.gmoorthy@gmail.com>
2024-11-08 00:47:43 +0100

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+	[HttpGet("userinfo")]
+	[Authorize] // Ensures only authenticated users can access this endpoint
+	public IActionResult GetUserInfo()
+	{
+		var user = HttpContext.User;
+		if (user?.Identity?.IsAuthenticated ?? false)
+		{
+			return Ok(new
+			{
+				Username = user.Identity.Name,
+				AuthenticationType = user.Identity.AuthenticationType
+			});
+		}
+		else
+		{
+			return Unauthorized("User is not authenticated");
+		}
+	}
+
+	[HttpGet("public")]
+	[AllowAnonymous] // Allows public access without authentication
+	public IActionResult GetPublicInfo()
+	{
+		return Ok("This is a public endpoint");
+	}
+}
